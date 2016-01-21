987d5d23ec9116f95d84df3655cab3a549db0b32
Alexander Hafstad <alexander@hafstad.dk>
2016-01-21 15:30:55 +0100

    Added nParsePushNotification module

---
+++ src/nParsePushNotifications.factory.js
@@ -0,0 +1,259 @@
+(function() {
+	"use strict";
+
+	angular
+		.module('nParsePushNotifications')
+		.factory('nParsePushNotifications', nParsePushNotifications);
+
+	/* @ngInject */
+	function nParsePushNotifications($ionicPlatform, $http, $q, $window, $rootScope, nParsePushNotificationsConfig) {
+
+		/**
+		 * Public API
+		 * @type object
+		 */
+		var service = {
+			init							: init,
+			unregister						: unregister,
+			hasPermission					: hasPermission,
+			setApplicationBadgeNumberIOS	: setApplicationBadgeNumberIOS,
+			subscribe						: subscribe,
+			unsubscribe						: unsubscribe
+		};
+
+		/**
+		 * Instance of PushNotifications
+		 * Isset then PushNotifications has been initialized
+		 * @type object
+		 */
+		var instance = undefined;
+
+		/**
+		 * Configuration for PushNotifications
+		 * And Parse.com
+		 * @type object
+		 */
+		var config = nParsePushNotificationsConfig;
+
+		return service;
+
+		/**
+		 * Initialze PushNotification - https://github.com/phonegap/phonegap-plugin-push
+		 * And uploads installation data to Parse.com
+		 * @returns Returns a promise
+		 */
+		function init() {
+			var deferred = $q.defer();
+
+			$ionicPlatform.ready(function deviceReady() {
+
+				if($window.PushNotification) {
+					instance = $window.PushNotification.init(config);
+
+					instance.on('registration', function registration(data) {
+						config.parse.deviceToken = data.registrationId;
+
+						_uploadParseInstallation().then(function uploadedParseInstallationSuccess(response) {
+							deferred.resolve(response);
+						}).catch(function uploadedParseInstallationCatch(response) {
+							deferred.reject(response);
+						});
+					});
+
+					instance.on('notification', function notification(notification) {
+						$rootScope.$emit('$cordovaPush:notificationReceived', notification);
+					});
+					instance.on('error', function notificationError(error) {
+						$rootScope.$emit('$cordovaPush:errorOccurred', error);
+					});
+
+				} else {
+					deferred.reject('Oops! Something went wrong: phonegap-plugin-push was not found!');
+				}
+			});
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Uploads installation data to Parse.com
+		 * @returns a promise
+		 * @private
+		 */
+		function _uploadParseInstallation() {
+			var deferred = $q.defer(),
+				parse    = config.parse,
+				reqData  = {
+					channels	: parse.channels,
+					deviceType	: parse.deviceType,
+					deviceToken : parse.deviceToken,
+					pushType	: parse.pushType,
+					GCMSenderId : parse.GCMSenderId
+				};
+
+			$http.post(config.parse.endpoint, reqData, {
+				"headers": {
+					"Content-Type"				: "application/json",
+					"X-Parse-Application-Id"	: parse.appID,
+					"X-Parse-REST-API-Key"		: parse.key
+				}
+			}).success(function uploadParseInstallationSuccess(response) {
+
+				if( response.hasOwnProperty('channels') ) {
+					parse.channels = response.channels;
+				}
+				parse.objectID = response.objectId;
+
+				deferred.resolve(response);
+
+			}).error(function uploadParseInstallationCatch(response, status) {
+				deferred.reject('Oops! Something went wrong', response, status);
+			});
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Updates installation data at Parse.com
+		 * @returns a promise
+		 * @private
+		 */
+		function _updateParseInstallation() {
+			var deferred = $q.defer(),
+				parse    = config.parse,
+				reqData  = {
+					channels	: parse.channels,
+					deviceType	: parse.deviceType,
+					deviceToken : parse.deviceToken,
+					pushType	: parse.pushType,
+					GCMSenderId : parse.GCMSenderId
+				};
+
+			if(parse.objectID) {
+
+				$http.put(parse.endpoint + '/' + parse.objectID, reqData, {
+						"headers": {
+							"Content-Type"				: "application/json",
+							"X-Parse-Application-Id"	: parse.appID,
+							"X-Parse-REST-API-Key"		: parse.key
+						}
+					})
+					.success(function updateParseInstallationSuccess(response) {
+						if( response.hasOwnProperty('channels') ) {
+							parse.channels = response.channels;
+						}
+						deferred.resolve(response);
+					})
+					.error(function updateParseInstallationCatch(response, status) {
+						deferred.reject('Oops! Something went wrong', response, status);
+					});
+			} else {
+				deferred.reject('PUSH NOT INIT');
+			}
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Unregister PushNotification - https://github.com/phonegap/phonegap-plugin-push
+		 * Beware that this cleans up all event handlers, so you will need to re-init service to get
+		 * Pushes working again without reloading the application.
+		 * @returns a promise
+		 */
+		function unregister() {
+			var deferred = $q.defer();
+
+			instance.unregister(function unregisterSuccess() {
+				deferred.resolve(true);
+			}, function unregisterCatch() {
+				deferred.reject('Oops! Something went wrong');
+			});
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Checks whether the push notification permission has been granted.
+		 * @returns a promise
+		 */
+		function hasPermission() {
+			var deferred = $q.defer();
+
+			$ionicPlatform.ready(function hasPermissionDeviceReady() {
+				if($window.PushNotification) {
+
+					$window.PushNotification.hasPermission(function getPermission(data) {
+						deferred.resolve(data.isEnabled);
+					});
+				} else {
+					deferred.reject('Oops! Something went wrong: phonegap-plugin-push was not found!');
+				}
+			});
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Set the badge count visible when the app is not running.
+		 * @param count
+		 */
+		function setApplicationBadgeNumberIOS(count) {
+			instance.setApplicationIconBadgeNumber(function setApplicationBadgeNumberSuccess() {
+				return count;
+			}, function setApplicationBadgeNumberCatch() {
+				return 'Oops! Something went wrong';
+			}, count);
+		}
+
+		/**
+		 * Subscribes to specified channel by updating channels at Parse.com
+		 * @param channel
+		 * @returns a promise
+		 */
+		function subscribe(channel) {
+			var deferred = $q.defer(),
+				parse    = config.parse;
+
+			if(instance) {
+				parse.channels[parse.channels.length] = channel;
+				_updateParseInstallation().then(function updateParseInstallationSuccess(response) {
+					deferred.resolve(response);
+				}).catch(function updateParseInstallationCatch(response) {
+					deferred.reject(response);
+				});
+			} else {
+				deferred.reject('Oops! Something went wrong: PushNotification does not seem to be initialized!');
+			}
+
+			return deferred.promise;
+		}
+
+		/**
+		 * Unsubscribes specified channel by updating channels at Parse.com
+		 * @param channel
+		 * @returns a promise
+		 */
+		function unsubscribe(channel) {
+			var deferred = $q.defer(),
+				parse 	 = config.parse;
+
+			if(instance) {
+				const index = parse.channels.indexOf(channel);
+				if(index >= 0) {
+					parse.channels.splice(index, 1);
+					_updateParseInstallation().then(function updateParseInstallationSuccess(response) {
+						deferred.resolve(response);
+					}).catch(function updateParseInstallationCatch() {
+						deferred.reject(response);
+					});
+				} else {
+					deferred.reject('Oops! Something went wrong: Channel does not seem to be subscribed');
+				}
+			} else {
+				deferred.reject('Oops! Something went wrong: PushNotification does not seem to be initialized!');
+			}
+
+			return deferred.promise;
+		}
+	}
+})();
---
+++ src/nParsePushNotifications.provider.js
@@ -0,0 +1,95 @@
+(function() {
+	"use strict";
+
+	angular
+		.module('nParsePushNotifications')
+		.provider('nParsePushNotificationsConfig', nParsePushNotificationsConfig);
+
+	/* @ngInject */
+	function nParsePushNotificationsConfig() {
+
+		/**
+		 * Get the current Platform to determine if Android or iOS
+		 */
+		var PLATFORM = _getPlatform();
+
+		function _getPlatform() {
+			if( ionic.Platform.isWebView() && ionic.Platform.isIOS() ) {
+				return 'ios';
+			} else if( ionic.Platform.isWebView() && ionic.Platform.isAndroid() ) {
+				return 'android';
+			}
+		}
+
+		var defaults = {
+			/**
+			 * Details about PushNotification options can be found here:
+			 * https://github.com/phonegap/phonegap-plugin-push/blob/master/docs/API.md#pushnotificationinitoptions
+			 */
+			android: {
+				senderID			: undefined,
+				icon				: undefined,
+				iconColor			: undefined,
+				sound				: true,
+				vibrate				: true,
+				clearNotifications	: true,
+				forceShow			: false,
+				topics				: []
+			},
+			ios: {
+				alert				: false,
+				badge				: false,
+				sound				: false,
+				clearBadge			: false,
+				senderID			: undefined,
+				gcmSandbox			: false,
+				topics				: []
+			},
+			parse: {
+				/**
+				 * Root endpoint for installations at Parse.com
+				 */
+				endpoint			: 'https://api.parse.com/1/installations',
+				/**
+				 * registrationId from PushNotifications
+				 */
+				objectID			: undefined,
+				/**
+				 * App ID for Parse.com
+				 */
+				appID				: undefined,
+				/**
+				 * REST-API-KEY for Parse.com
+				 */
+				key					: undefined,
+				deviceType			: PLATFORM,
+				pushType 			: PLATFORM === 'android' ? 'gcm' : undefined,
+				GCMSenderId         : undefined,
+				/**
+				 * Device token from Parse.com
+				 */
+				deviceToken			: undefined,
+				/**
+				 * Channels for Parse.com
+				 */
+				channels			: [""]
+			}
+		};
+
+		this.configure = function(config) {
+
+			for(var key in defaults) {
+				if( config.hasOwnProperty(key) ) {
+					angular.extend(defaults[key], config[key]);
+					if( config[key].hasOwnProperty('senderID') ) {
+						defaults.parse.GCMSenderId = config[key].senderID;
+					}
+				}
+			}
+		};
+
+		this.$get = [function() {
+			return defaults;
+		}];
+	}
+})();
